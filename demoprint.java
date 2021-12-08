77e790f3e4c662bc893d135ada620ff90bbf6fc1
kirilllen <90799887+kirilllen@users.noreply.github.com>
2021-12-08 22:07:14 +0400

    Erase file

--- demoprint.java
+++ demoprint.java
@@ -3,10 +3,7 @@
 public class demoprint {
   public static void main(String[] args){
 
-      UserDataRequest req1=new UserDataRequest();
-      System.out.println("Здравствуйте, господин "+req1.GetName());
-    
-      Captcha cap1=new Captcha();
+
     //сначала имя - потом капча
   }
 }
