75f9d11b3cdef43b614a063fcb75417e92cc61e2
kirilllen <90799887+kirilllen@users.noreply.github.com>
2021-12-08 22:04:33 +0400

    Adding "Mr" in name

--- demoprint.java
+++ demoprint.java
@@ -4,7 +4,8 @@ public class demoprint {
   public static void main(String[] args){
 
       UserDataRequest req1=new UserDataRequest();
-      System.out.println("Здравствуйте, "+req1.GetName());
+      System.out.println("Здравствуйте, господин "+req1.GetName());
+    
       Captcha cap1=new Captcha();
     //сначала имя - потом капча
   }
