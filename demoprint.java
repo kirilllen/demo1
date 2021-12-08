c3af54845e9aa01f6bf460f5d3ebddea3966887c
Kirill Tapkov <izhjup@mail.ru>
2021-12-08 21:02:14 +0400

    First local commit (full)

--- demoprint.java
+++ demoprint.java
@@ -1,5 +1,8 @@
+
+
 public class demoprint {
-  public void main(String args){
-   System.out.println("Ничего полезного"); 
+  public static void main(String[] args){
+
+      System.out.println("Ничего полезного");
   }
 }
